789e8f7e0c07a4e4b4e2030c84b055979b7e2de0
gooking <javazj@qq.com>
2023-08-06 10:24:05 +0800

    2023-08-06

--- lib/apifm.dart
+++ lib/apifm.dart
@@ -630,6 +630,12 @@ orderReputation(data) {
 orderReputationList(data) {
   return request('/order/listReputation', true, 'post', data);
 }
+orderReputationDelete(data) {
+  return request('/order/reputation/delete', true, 'post', data);
+}
+orderReputationModify(data) {
+  return request('/order/reputation/modify', true, 'post', data);
+}
 orderClose(token, orderId) {
   return request('/order/close', true, 'post', {
     'orderId': orderId,
@@ -2206,4 +2212,34 @@ listingRemoveGoods(data) {
 }
 listingJoinList(data) {
   return request('/listingInfo/joinList', true, 'post', data);
+}
+attendantAcceptOrder(data) {
+  return request('/order/acceptOrder', true, 'post', data);
+}
+orderCancelOrderPeriod(data) {
+  return request('/order/cancelOrderPeriod', true, 'post', data);
+}
+orderStartOrderPeriod(data) {
+  return request('/order/startOrderPeriod', true, 'post', data);
+}
+userAttendantOrderRejectOrder(token, orderId) {
+  return request('/order/rejectOrder', true, 'post', { token, orderId });
+}
+userAttendantOrderServing(token, orderId) {
+  return request('/order/serving', true, 'post', { token, orderId });
+}
+shansongCourierInfo(data) {
+  return request('/order/shansongCourierInfo', true, 'get', data);
+}
+shansongOrderInfo(data) {
+  return request('/order/shansongOrderInfo', true, 'get', data);
+}
+userAttendantOrderStatistics(data) {
+  return request('/order/statisticsJishi', true, 'get', data);
+}
+keloopOrderLogs(data) {
+  return request('/order/keloop/orderLogs', true, 'get', data);
+}
+keloopCourierTag(data) {
+  return request('/order/keloop/courierTag', true, 'get', data);
 }
