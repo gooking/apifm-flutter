9154daf6ed96453409621db4295604944776e907
gooking <javazj@qq.com>
2024-09-22 14:51:50 +0800

    增加stripe支付接口

--- lib/apifm.dart
+++ lib/apifm.dart
@@ -130,6 +130,9 @@ scoreRank(data) {
 scoreRankBydate(data) {
   return request('/score/rankBydate', true, 'get', data);
 }
+scoreMyStatistics(data) {
+  return request('/score/myStatistics', true, 'get', data);
+}
 scoreTaskList(token) {
   return request('/score/taskList', true, 'get', { token });
 }
@@ -672,6 +675,9 @@ orderClose(token, orderId) {
     'orderId': orderId,
     'token': token
   });
+}
+orderCloseV2(data) {
+  return request('/order/close', true, 'post', data);
 }
 orderDelete (token, orderId) {
   return request('/order/delete', true, 'post', {
@@ -2070,6 +2076,19 @@ jicunGoodsList(data) {
 }
 jicunGoodsDetail(data) {
   return request('/jicunGoods/detail', true, 'get', data);
+}
+// stripe
+stripeAddCard(data) {
+  return request('/pay/stripe/addCard', true, 'post', data);
+}
+stripeCardList(token) {
+  return request('/pay/stripe/cardList', true, 'get', { token });
+}
+stripeDelCard(token, cardId) {
+  return request('/pay/stripe/deleteCard', true, 'post', { token, cardId });
+}
+stripeCharge(data) {
+  return request('/pay/stripe/charge', true, 'post', data);
 }
 // ocr
 ocrBusinessLicense(imageUrl) {
